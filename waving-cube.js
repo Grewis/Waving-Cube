28c7e9955cc8821b6b3d7db5380218f626d77e4f
Mohan Grewis <mohan.grewis@epitech.eu>
2019-03-14 21:47:03 +0100

    Start rendering Web Gl

--- waving-cube.js
+++ waving-cube.js
@@ -1,17 +1,37 @@
 let angle = 0;
+let w = 20
+let h = 20;
+let ma = atan(1 / sqrt(2));
 
 function setup() {
-	createCanvas(400,400)
+	createCanvas(400,400, WEBGL)
+	rotateX((radians(50)));
 }
 
 function draw() {
 	background(255)
-	translate(width / 2, height / 2);
-	rectMode(CENTER);
+	ortho(-300, 300, -300, 300, -1000, 1000); 
+	//translate(width / 2, height / 2);
+	rotateX((radians(-20)));
+	rotateY((radians(-45)));
+	let maxD = dist(0, 0, 200, 200);
 
-	fill(0);
-	let h = map(sin(angle), -1, 1, 0, 100);
-	rect(0, 0, 10, h);
+
+
+	for (let z = 0; z < height; z += h) {
+		for (let x = 0; x < width; x += w) {
+			push();
+			let d = dist(x,z,width/2, height/2);
+			let offset = map (d, 0, maxD, -1, 1);
+			let a = angle + offset;
+			let h = map(sin(a), -1, 1, 0, 300);
+			translate(x - width / 2, 0, z - height / 2);
+			normalMaterial();
+			box(w -2 , h, w-2 );
+			//rect(x - width / 2 + w / 2, 0,  w - 2, h);
+			pop();
+		}
+	}
 
 	angle += 0.1;
 }
